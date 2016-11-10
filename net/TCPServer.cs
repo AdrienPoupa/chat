790747a5f66f6171b48f58bfd999f1d1c7bb2efd
fabien beaujean <fabienbeaudimi@hotmail.fr>
2016-11-10 12:30:46 +0100

    net

--- net/TCPServer.cs
+++ net/TCPServer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace chat.net
@@ -10,18 +12,25 @@ namespace chat.net
     abstract class TCPServer
     {
         private Socket commSocket;
-        private Socket waitSock; // todo: serversocket in the subject
+        private Socket waitSocket; // todo: serversocket in the subject
         private int _port; // todo: get port from socket?
 
         public void startServer(int port)
         {
-            // todo
-            _port = port;
+            IPHostEntry ipHostEntry = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+
+            waitSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            waitSocket.Bind(new IPEndPoint(ipAddress, 8000));
+            waitSocket.Listen(2000);
+
+            Thread t = new Thread(new ThreadStart(this.run));
+            t.Start();
         }
 
         public void stopServer()
         {
-            // todo
+            waitSocket.Close();
         }
 
         public void run()
