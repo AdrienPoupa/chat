3713b6d280572507874bda86cb334537eddc1d8c
fabien beaujean <fabienbeaudimi@hotmail.fr>
2016-11-18 11:11:24 +0100

    Class program to start both server and client

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using chat.test;
+
+namespace chat
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Server.Main();
+            Client.Main();
+        }
+    }
+}
