63a0d0c5dba3d5611380f7bba4f32a29d92a775f
fabien beaujean <fabienbeaudimi@hotmail.fr>
2016-11-18 10:19:45 +0100

    fuck this shit

---
+++ test/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using chat.client;
+
+namespace chat.test
+{
+    class Server
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
